99a55fe8814a5abe8bb2042b12da3748ad191208
dookda <sakda.homhuan@gmail.com>
2025-05-22 18:26:24 +0700

    clear map

--- nodejs/www/rectify/app.js
+++ nodejs/www/rectify/app.js
@@ -53,399 +53,18 @@ const overlayMaps = {
 
 L.control.layers(baseLayers, overlayMaps).addTo(map);
 
-// Configure Geoman controls
-map.pm.addControls({
-    position: 'topleft',
-    drawCircle: false,
-    drawMarker: false,
-    drawPolyline: false,
-    drawRectangle: false,
-    drawPolygon: false,
-    editMode: false,
-    dragMode: false,
-    cutPolygon: false,
-    removalMode: false,
-    rotateMode: false,
-    drawText: false,
-    drawCircleMarker: false,
-});
-
-// Area calculation utilities
-const formatArea = (area) => {
-    return area >= 1e6
-        ? `${(area / 1e6).toLocaleString(undefined, { maximumFractionDigits: 2 })} km²`
-        : `${area.toLocaleString(undefined, { maximumFractionDigits: 2 })} m²`;
-};
-
-// Label management
-const updateAreaLabel = async (layer) => {
-    try {
-        const geojsonFeature = layer.toGeoJSON();
-
-        const res = await fetch(`/rub/api/area`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ geometry: geojsonFeature.geometry })
-        });
-
-        if (!res.ok) {
-            const errText = await res.text();
-            throw new Error(`API error ${res.status}: ${errText}`);
-        }
-
-        const { area } = await res.json();
-        const xls_sqm = document.getElementById('xls_sqm').value;
-
-        document.getElementById('shparea_sqm').value = area.toFixed(0);
-        const diff = Math.abs(area - xls_sqm);
-
-        if (diff >= 100) {
-            document.getElementById('message').innerHTML = '<h5><span class="badge bg-danger">เนื้อที่ยังไม่เท่ากัน</span></h5>';
-        } else {
-            document.getElementById('message').innerHTML = '<h5><span class="badge bg-success">เนื้อที่ใกล้เคียงกัน</span></h5>';
-        }
-    } catch (error) {
-        console.error('Error updating label:', error);
-    }
-};
-
-function showFeaturePanel(feature, layer) {
-    const xls = Number(feature.properties.xls_sqm);
-    const id = document.getElementById('id');
-    const xls_app_no = document.getElementById('xls_app_no');
-    const xls_sqm = document.getElementById('xls_sqm');
-    const refinal = document.getElementById('refinal');
-
-    id.value = feature.properties.id;
-    xls_app_no.value = feature.properties.app_no;
-    xls_sqm.value = feature.properties.xls_sqm;
-    refinal.value = feature.properties.refinal;
-
-    // console.log(feature.properties);
-    updateAreaLabel(layer);
-}
-
-const getFeatureStyle = (feature) => {
-    const xls = Number(feature.properties.xls_sqm);
-    const shp = Number(feature.properties.shparea_sqm);
-    const diff = xls - shp;
-    const isEqual = Math.abs(diff) <= 100;
-
-    return {
-        color: isEqual ? '#00cc00' : '#ca0020',
-        weight: 2,
-        opacity: 0.9,
-        fillColor: isEqual ? '#90ee90' : '#f4a582',
-        fillOpacity: 0.1
-    };
-};
-
-const onEachFeature = (feature, layer) => {
-    layer.bindPopup(`${feature.properties.id}`);
-
-    layer.on('click', () => {
-        // map.fitBounds(layer.getBounds());
-        showFeaturePanel(feature, layer);
-        featureGroup.eachLayer(l => l.pm.disable());
-        layer.pm.enable();
-
-        selectedLayer = layer;
-    });
-
-    layer.on('pm:edit pm:dragend pm:update pm:change', () => updateAreaLabel(layer));
-}
-
-var selectedLayer = null;
-const loadGeoData = async () => {
-    try {
-        const tb = document.getElementById('tb').value;
-        const response = await fetch(`/rub/api/getfeatures/${tb}`);
-        const { data } = await response.json();
-
-        const tableData = data.map(item => ({
-            id: item.id,
-            refinal: item.refinal,
-            farm_name: item.farm_name,
-            f_name: item.f_name,
-            l_name: item.l_name,
-            age: item.age,
-            geom: JSON.parse(item.geom),
-            app_no: item.app_no,
-            xls_sqm: item.xls_sqm,
-            shparea_sqm: item.shparea_sqm,
-            classified: item.classified,
-        }));
-
-        const dataTable = $('#featureTable').DataTable({
-            data: tableData,
-            columns: [
-                {
-                    data: null,
-                    title: 'Zoom',
-                    render: (data, type, row) => {
-                        const _geojson = JSON.stringify(row.geom);
-                        return `<a class="btn btn-success map-btn" 
-                                    data-refid="${row.id}" 
-                                    data-geojson='${_geojson}'
-                                    href="#">
-                                    <em class="icon ni ni-zoom-in"></em>&nbsp;ซูม
-                                </a>`
-                    }
-                },
-                { data: 'id', title: 'ID' },
-                { data: 'farm_name', title: 'farm_name' },
-                { data: 'f_name', title: 'f_name' },
-                { data: 'l_name', title: 'l_name' },
-                { data: 'age', title: 'age' },
-                { data: 'app_no', title: 'Application No' },
-                { data: 'xls_sqm', title: 'เนื้อที่เป้าหมาย (m²)' },
-                {
-                    data: 'shparea_sqm',
-                    title: 'เนื้อที่ขณะนี้ (m²)',
-                    render: (data, type, row) => Number(data).toFixed(0)
-                },
-                {
-                    data: null,
-                    title: 'ตรวจสอบ (m²)',
-                    render: (data, type, row) => {
-                        const xls = Number(data.xls_sqm);
-                        const shp = Number(data.shparea_sqm);
-                        const diff = xls - shp;
-                        const color = Math.abs(diff) <= 100 ? 'green' : 'red';
-                        const diffStyle = `color: ${color}; font-weight: bold;`;
-                        return `<span style="${diffStyle}">
-                                    ${Math.abs(diff) <= 100 ? "เนื้อที่ถูกต้อง" : "เนื้อที่ไม่ถูกต้อง"}
-                                    (${diff.toLocaleString(undefined, { maximumFractionDigits: 1 })})
-                                </span>`;
-                    }
-                },
-                {
-                    data: 'classified',
-                    title: 'Classified',
-                    render: (data, type, row) => {
-                        const color = data ? 'green' : 'red';
-                        const diffStyle = `color: ${color}; font-weight: bold;`;
-                        return `<span style="${diffStyle}">${data ? "classify แล้ว" : "ยังไม่ classify"}</span>`;
-                    }
-                }
-            ],
-            pageLength: 10,
-            responsive: false,
-            select: true,
-            destroy: true,
-            scrollX: true,
-        });
-
-        const updateMap = () => {
-            featureGroup.clearLayers(); // Clear existing layers
-            const visibleRows = dataTable.rows({ search: 'applied' }).data().toArray();
-
-            visibleRows.forEach(row => {
-                const geoJsonData = {
-                    type: 'Feature',
-                    geometry: row.geom,
-                    properties: {
-                        id: row.id,
-                        refinal: row.refinal,
-                        app_no: row.app_no,
-                        xls_sqm: row.xls_sqm,
-                        shparea_sqm: row.shparea_sqm
-                    }
-                }
-
-                L.geoJson(geoJsonData, {
-                    style: getFeatureStyle,
-                    onEachFeature: onEachFeature,
-                }).addTo(featureGroup);
-            });
-        };
-
-        updateMap();
-
-        dataTable.on('draw', () => {
-            updateMap();
-        });
-
-        $('#featureTable tbody').on('click', '.map-btn', function (e) {
-            try {
-                e.stopPropagation();
-                const geojson = $(this).data('geojson');
-                const layer = L.geoJSON(geojson)
-
-                const bounds = layer.getBounds();
-                map.fitBounds(bounds, {
-                    padding: [20, 20],
-                    // maxZoom: 16         
-                });
-                selectedLayer = layer;
-            } catch (error) {
-                console.error('Failed to parse GeoJSON:', error);
-            }
-        });
-
-
-        dataTable.rows().every(function () {
-            const rowData = this.data();
-            $(this.node()).attr('id', `row_${rowData.id}`);
-        });
-
-    } catch (error) {
-        console.error('Error loading data:', error);
-        alert('Failed to load spatial data');
-    }
-};
-
-map.on('click', (e) => featureGroup.eachLayer(l => l.pm.disable()));
-
-document.getElementById('save').addEventListener('click', async () => {
-    if (!selectedLayer) {
-        alert('กรุณาเลือกแปลงที่ต้องการบันทึกก่อน');
-        return;
-    }
-
-    const id = document.getElementById('id').value
-    const refinal = document.getElementById('refinal').value;
-    const displayName = document.getElementById('displayName').value;
-
-    const features = [];
-    features.push(selectedLayer.toGeoJSON());
-
-    try {
-        const tb = document.getElementById('tb').value;
-        const response = await fetch(`/rub/api/updatefeatures/${tb}`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id, refinal, features, displayName })
-        });
-        const result = await response.json();
-        alert(`อัพเดท features ${result.updated} เรียบร้อย`);
-
-        if (result.success) {
-            featureGroup.eachLayer(layer => {
-                layer.pm.disable();
-                layer.areaLabel?.remove();
-            });
-
-            featureGroup.clearLayers();
-            loadGeoData();
-        } else {
-            alert('Failed to update features');
-        }
-    } catch (error) {
-        console.error('Error saving data:', error);
-        alert('Failed to save data');
-    }
-});
-
-document.getElementById("restore").addEventListener("click", () => {
-    try {
-        const modal = document.getElementById("restoreModal");
-        if (modal) {
-            const bsModal = new bootstrap.Modal(modal);
-            bsModal.show();
-        } else {
-            console.error(`Modal with ID ${modalId} not found.`);
-        }
-    } catch (error) {
-        console.error('Failed to fetch user:', err);
-    }
-})
-
-document.getElementById('btnRestore').addEventListener("click", async () => {
-    try {
-        const tb = document.getElementById('tb').value;
-        const id = document.getElementById('restoreId').value;
-        const response = await fetch(`/rub/api/restorefeatures/${tb}/${id}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id })
-        });
-        const result = await response.json();
-        alert(`อัพเดท features ${result.updated} เรียบร้อย`);
-
-        if (result.success) {
-            featureGroup.eachLayer(layer => {
-                layer.pm.disable();
-                layer.areaLabel?.remove();
-            });
-
-            featureGroup.clearLayers();
-            loadGeoData();
-            document.getElementById('restoreId').value = "";
-            const modal = document.getElementById("restoreModal");
-            if (modal) {
-                const bsModal = bootstrap.Modal.getInstance(modal);
-                bsModal.hide();
-            } else {
-                console.error(`Modal with ID ${modalId} not found.`);
-            }
-        } else {
-            alert('Failed to update features');
-        }
-    } catch (error) {
-        console.error('Error restoring data:', error);
-        alert('Failed to restore data');
-    }
-});
-
-document.getElementById('classify').addEventListener('click', () => {
-    const id = document.getElementById('id').value;
-    if (!id) {
-        alert('เลือกแปลงที่ต้องการ classify ก่อน');
-        return;
-    }
-    const tb = document.getElementById('tb').value;
-    fetch(`/rub/api/create_reclass_feature/${tb}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id })
-    }).then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                window.open(`/rub/reclass/index.html?tb=${tb}&id=${id}`, '_self');
-            } else {
-                alert('Failed to create reclassification layer');
-            }
-        }).catch(error => {
-            console.error('Error creating reclassification layer:', error);
-            alert('Failed to create reclassification layer');
-        });
-});
-
 document.getElementById('dashboard').addEventListener('click', (e) => {
     e.preventDefault();
     const tb = document.getElementById('tb').value;
     window.location.href = './../reclassdash/index.html?tb=' + tb;
 });
 
-const initApp = async () => {
-    try {
-        const urlParams = new URLSearchParams(window.location.search);
-        const tb = urlParams.get('tb');
-        if (!tb || tb === 'undefined') {
-            alert('พื้นที่ไม่ถูกต้อง');
-            window.location.href = './../index.html';
-        } else {
-            document.getElementById('tb').value = tb;
-            await loadGeoData();
-            map.fitBounds(featureGroup.getBounds());
-        }
-    } catch (error) {
-        console.error('Error loading data:', error);
-    }
-};
-
 window.addEventListener('DOMContentLoaded', async () => {
     try {
         const response = await fetch('/rub/api/ldd_getprovince');
-
         const provinces = await response.json();
-
-        console.log(provinces.result);
-
         const provinceSelect = document.getElementById('provinceSelect');
 
-        // Clear loading message
         provinceSelect.innerHTML = '<option selected disabled>Select a province</option>';
 
         provinces.result.forEach(province => {
@@ -518,9 +137,10 @@ async function loadParcelData(provinceId, amphoeId, pacelNumber) {
                     layer.bindPopup('Parcel seq: ' + feature.properties.parcel_seq);
                 }
             }
-        }).addTo(map);
+        });
 
-        // Zoom to the parcel bounds
+        featureGroup.addLayer(parcelLayer);
+
         if (parcelLayer.getBounds().isValid()) {
             map.fitBounds(parcelLayer.getBounds());
         } else {
@@ -539,7 +159,6 @@ document.getElementById('searchButton').addEventListener('click', async function
     const amphoeId = document.getElementById('amphoeSelect').value;
     const pacelNumber = document.getElementById('pacelNumber').value
 
-    console.log(provinceId, amphoeId, pacelNumber);
     if (!provinceId || !amphoeId) {
         return;
     }
@@ -551,7 +170,20 @@ document.getElementById('searchButton').addEventListener('click', async function
         const tambonSelect = document.getElementById('tambonSelect');
         tambonSelect.innerHTML = '<option selected disabled>Error loading tambons</option>';
     }
+});
 
+document.getElementById('clearButton').addEventListener('click', function () {
+    const provinceSelect = document.getElementById('provinceSelect');
+    const amphoeSelect = document.getElementById('amphoeSelect');
+    const pacelNumber = document.getElementById('pacelNumber');
+
+    provinceSelect.value = '';
+    amphoeSelect.value = '';
+    pacelNumber.value = '';
+
+    // Clear the map
+    featureGroup.clearLayers();
+    // document.getElementById()
 });
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -566,7 +198,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             document.getElementById('display-name').textContent = user.displayName;
             document.getElementById('displayName').value = user.displayName;
 
-            await initApp();
+            // await initApp();
 
             document.getElementById('logout-link').addEventListener('click', async (e) => {
                 e.preventDefault();
